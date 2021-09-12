84f366629caa373ec38ce33d10a3c39c6c756820
Erick Saavedra <esvdrah@gmail.com>
2021-09-12 12:07:06 -0500

    Done color-flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// Array to store colors
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+// HTML variables
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Listening for a "click" on the btn variable
+btn.addEventListener('click', function () {
+	// Get random number between 0 - 3
+	const randomNumber = getRandomNumber();
+
+	// Changing the styles with the randomly obtained color
+	document.body.style.backgroundColor = colors[randomNumber];
+	color.textContent = colors[randomNumber];
+});
+
+// Function that generates a random number by the size of array => colors[]
+function getRandomNumber() {
+	return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,26 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// Array to generate hexadecimal code
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+// HTML variables
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Listening for a "click" on the btn variable
+btn.addEventListener('click', function () {
+	// Array to store the hexadecimal code
+	let hexColor = '#';
+
+	// Loop for generate 6 random numbers and concatenate them
+	for (let i = 0; i < 6; i++) {
+		hexColor += hex[getRandomNumber()];
+	}
+
+	// Changing the styles with the generated code
+	color.textContent = hexColor;
+	document.body.style.backgroundColor = hexColor;
+});
+
+// Function that generates a random number by the size of array => hex[]
+function getRandomNumber() {
+	return Math.floor(Math.random() * hex.length);
+}
