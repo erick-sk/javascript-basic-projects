8ff167f912a90c615511cd6ef11e06a9985c1dc3
Erick Saavedra <esvdrah@gmail.com>
2021-09-14 13:58:47 -0500

    Done navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -1,5 +1,19 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// HTML variables
+const links = document.querySelector('.links');
+const navToggle = document.querySelector('.nav-toggle');
+
+// listening for one click on navToggle
+navToggle.addEventListener('click', showLinks);
+
+// Function displaying links
+function showLinks() {
+	// Example based on conditions
+	// if (!links.classList.contains('show-links')) {
+	// 	links.classList.add('show-links');
+	// } else {
+	// 	links.classList.remove('show-links');
+	// }
+
+	// .toggle
+	links.classList.toggle('show-links');
+}
