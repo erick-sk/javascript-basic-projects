ab8d8f9c08e94b73147de605bd0ef4c31f0f111d
Erick Saavedra <esvdrah@gmail.com>
2021-09-15 15:08:21 -0500

    Done questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,35 @@
-//using selectors inside the element
-// traversing the dom
+// Using selectors inside the element
+
+// HTML tags
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+	// button plus tag
+	const btn = question.querySelector('.question-btn');
+
+	// listening click, add and filter class
+	btn.addEventListener('click', function () {
+		questions.forEach(function (item) {
+			if (item !== question) {
+				item.classList.remove('show-text');
+			}
+		});
+		question.classList.toggle('show-text');
+	});
+});
+
+// Traversing the dom
+
+// HTML tags
+// const btns = document.querySelectorAll('.question-btn');
+
+// listening for click on each btn
+// btns.forEach(function (btn) {
+// 	btn.addEventListener('click', (e) => {
+// 		//accessing the parent node
+// 		const question = e.currentTarget.parentElement.parentElement;
+
+// 		// show and hide text
+// 		question.classList.toggle('show-text');
+// 	});
+// });
